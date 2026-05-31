c3482a18e47fbafd4aab966d7f3a8efe1cd7e216
GuilhermePerelra <g.pereira@estudante.ifmt.edu.br>
2026-05-31 17:47:55 -0400

    websocket handling

--- terchum/ws/RoomManager.cs
+++ terchum/ws/RoomManager.cs
@@ -10,7 +10,12 @@ public class RoomManager
     public void AddConnection(string roomName, WebSocket webSocket)
     {
         ConcurrentBag<WebSocket> room = _rooms.GetOrAdd(roomName, _=> new ConcurrentBag<WebSocket>());
-        
         room.Add(webSocket);
+    }
+    
+    public ConcurrentBag<WebSocket>? GetRoom(string roomName)
+    {
+        _rooms.TryGetValue(roomName, out var room);
+        return room;
     }
 }
--- terchum/ws/WebSocketController.cs
+++ terchum/ws/WebSocketController.cs
@@ -1,4 +1,6 @@
+using System.Collections.Concurrent;
 using System.Net.WebSockets;
+using System.Text;
 
 namespace terchum.ws;
 
@@ -26,11 +28,46 @@ public static class WebSocketController
 private static async Task HandleConnection(WebSocket webSocket, RoomManager roomManager, string room)
     {
         var buffer = new byte[1024];
-        
+
         while (webSocket.State == WebSocketState.Open)
+        {
+            var result = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
+
+            if (result.MessageType == WebSocketMessageType.Close)
             {
-                var result = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
+                await webSocket.CloseAsync(
+                    WebSocketCloseStatus.NormalClosure,
+                    "Fechado",
+                    CancellationToken.None);
+                
+                break;
             }
+            
+            var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+            
+            await Broadcast(roomManager, room, message);
+        }
+    }
+
+    private static async Task Broadcast(RoomManager roomManager, string roomName, string message)
+    {
+        ConcurrentBag<WebSocket> sockets = roomManager.GetRoom(roomName);
+
+        if (sockets == null) return;
+        
+        var bytes = Encoding.UTF8.GetBytes(message);
+        foreach (var socket in sockets)
+        {
+            if (socket.State == WebSocketState.Open)
+            {
+                await socket.SendAsync(bytes, WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+        }
     }
 
 }
+
+/* TODO*
+    1. Salvamento e criação dinamica de salas
+    2. Carregar mensagens do db
+*/
